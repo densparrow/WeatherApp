6089bcb2fda42ff5aaf54c2dca3d2417d7486d3a
Dzianis Bohush <110992506+densparrow@users.noreply.github.com>
2022-08-28 17:44:27 +0300

    применил шаблон bootstrap для index.html получил APPI для для получения информации о погоде и ввел получение запроса во views.py

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
 def index(request):
+    appid = 'bb7dc3d70ae900cd335cdd42c1de4025'
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'Minsk'
+    res = requests.get(url.format(city))
+    print(res.text)
+
     return render(request, 'weather/index.html')
 
 
