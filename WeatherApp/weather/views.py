d37c23297f64907eb1e35721e62561344e989668
Dzianis Bohush <110992506+densparrow@users.noreply.github.com>
2022-09-03 18:56:27 +0300

    Вывел информацию о погоде на странице c index.html

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 import requests
 
 
-# Create your views here.
 def index(request):
     appid = 'bb7dc3d70ae900cd335cdd42c1de4025'
 
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
     city = 'Minsk'
-    res = requests.get(url.format(city))
-    print(res.text)
+    res = requests.get(url.format(city)).json()
 
-    return render(request, 'weather/index.html')
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+    }
+
+    context = {'info': city_info}
+    return render(request, 'weather/index.html', context)
 
 
 def new(request):
