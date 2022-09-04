9053000b0e7acc5f17ffd3f0021735803fe2d358
Dzianis Bohush <110992506+densparrow@users.noreply.github.com>
2022-09-04 18:48:56 +0300

    Вывел информацию о погоде на new.html

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -21,4 +21,18 @@ def index(request):
 
 
 def new(request):
-    return render(request, 'weather/new.html')
+    appid = 'bb7dc3d70ae900cd335cdd42c1de4025'
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    city = 'Minsk'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+    }
+
+    context = {'info': city_info}
+    return render(request, 'weather/new.html', context)
